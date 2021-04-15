4a8e4b750e723d973e2634967281eabd4cdcab26
juliencaesar69 <71985491+juliencaesar69@users.noreply.github.com>
2021-04-15 20:56:07 +0300

    Add files via upload

---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public Saves _Saves;
+
+    public GameObject Warning_Panel;
+    public GameObject ErrorPanel;
+
+    public void New_Start()
+    {
+        if (_Saves._SavePoz == 0)
+        {
+            SceneManager.LoadScene (1);
+        }
+        else if (_Saves._SavePoz != 0)
+        {
+            Warning_Panel.SetActive (true);
+        }
+    }
+
+    public void ResetStart()
+    {
+        _Saves.ResetSaves();
+        SceneManager.LoadScene (1);
+    }
+
+    public void LoadSave()
+    {
+        if(_Saves._SavePoz != 0)
+        {
+            SceneManager.LoadScene (1);
+        }
+        else if(_Saves._SavePoz == 0)
+        {
+            ErrorPanel.SetActive (true);
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
